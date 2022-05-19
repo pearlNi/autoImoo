bcef929a7d859395664ff5fab489116ffc955e8f
pearlNi <927082374@qq.com>
2022-05-19 17:33:52 +0800

    loginFail case

--- testCases/HtmlReport.py
+++ testCases/HtmlReport.py
@@ -2,22 +2,25 @@
 import unittest
 import HTMLTestRunner
 from time import strftime, localtime, time
-from loginSuccess import LoginTestCase
+from loginSuccess import *
+from loginFail import *
 import sys
 reload(sys)
 sys.setdefaultencoding('utf8')
 
-suite = unittest.TestSuite()
-suite.addTest(LoginTestCase("test_login"))
-# 把测试用例添加到测试容器中
+# suite = unittest.TestSuite()
+suite1 = unittest.TestLoader().loadTestsFromTestCase(LoginSuccessTestCase)
+suite2 = unittest.TestLoader().loadTestsFromTestCase(LoginFailTestCase)
+suite = unittest.TestSuite([suite1, suite2])
 
-now = strftime("%Y-%m-%M-%H_%M_%S", localtime(time()))
+# 把测试用例添加到测试容器中
+now = strftime("%Y-%m-%M-%H_%M_%S", localtime())
 # 获取当前时间
 filename = now + "test.html"
 # 文件名
 
 fp = file(filename, 'wb')
-# 以二进制的方式打开文件并写入结果
+
 runner = HTMLTestRunner.HTMLTestRunner(
     stream=fp,
     verbosity=2,
@@ -25,5 +28,3 @@ runner = HTMLTestRunner.HTMLTestRunner(
     description="测试报告的详情")
 
 runner.run(suite)
-
-# fp.close()
--- testCases/loginFail.py
+++ testCases/loginFail.py
@@ -6,24 +6,46 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.common.keys import Keys
 import time
 
-class LoginTestCase(unittest.TestCase):
+class LoginFailTestCase(unittest.TestCase):
     def setUp(self):
         self.driver = webdriver.Chrome()
         self.driver.maximize_window()
         self.driver.get("https://www.imooc.com/")
         self.driver.implicitly_wait(15)
-
-    def test_login(self):
+    # 用户名、密码为空
+    def test_login_null(self):
         self.driver.find_element_by_id('js-signin-btn').click()
-        time.sleep(10)
-        uName_tag = self.driver.find_element_by_name("email")
-        uPwd_tag = self.driver.find_element_by_name("password")
         self.driver.find_element_by_class_name('xa-login').click()
-        time.sleep(10)
+        time.sleep(5)
+        result = self.driver.find_element_by_css_selector('p.errorHint').text
+        respect_result = u'请输入正确的邮箱或手机号'
+        self.assertEqual(result, respect_result)
+    # 密码为空
+    def test_login_nPwd(self):
+        self.driver.find_element_by_id('js-signin-btn').click()
+        self.driver.find_element_by_name("email").send_keys('12345678@qq.com')
+        self.driver.find_element_by_class_name('xa-login').click()
+        time.sleep(5)
+        result = self.driver.find_element_by_xpath("//form[@id='signup-form']/div[2]/p").text
+        respect_result = u'请输入6-20位密码，区分大小写，不能使用空格！'
+        self.assertEqual(result, respect_result)
+    # 密码错误
+    def test_login_fPwd(self):
+        self.driver.find_element_by_id('js-signin-btn').click()
+        self.driver.find_element_by_name("email").send_keys('12345678@qq.com')
+        time.sleep(5)
+        self.driver.find_element_by_name("password").send_keys('12345678@qq.com')
+        time.sleep(5)
+        self.driver.find_element_by_class_name('xa-login').click()
+        time.sleep(5)
+        result = self.driver.find_element_by_class_name('rlf-tip-error').text
+        respect_result = u'请求超时'
+        print('result' + result)
+        print('respect_result' + respect_result)
+        self.assertEqual(result, respect_result)
 
     def tearDown(self):
         self.driver.close()
 
 if __name__ == '__main__':
-    # test_login('927082374@qq.com', 'Niyanzhu007.')
     unittest.main()
